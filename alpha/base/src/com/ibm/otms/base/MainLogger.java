357377bab37a913cd1395cc3443f6ff54131a0c0
de131906 <88097401+de131906@users.noreply.github.com>
2024-09-05 09:09:05 +0200

    added project dependency ots-base - otms-loggerbase

---
+++ alpha/base/src/com/ibm/otms/base/MainLogger.java
@@ -0,0 +1,12 @@
+package com.ibm.otms.base;
+
+import com.ibm.otms.loggerbase.Greeter;
+
+public class MainLogger {
+
+	public static void main(String[] args) {
+
+		Greeter greeter = new Greeter();
+		greeter.greet();
+	}
+}
---
+++ alpha/loggerbase/src/com/ibm/otms/loggerbase/Greeter.java
@@ -0,0 +1,8 @@
+package com.ibm.otms.loggerbase;
+
+public class Greeter {
+
+	public void greet() {
+		System.out.println("Hello, World");
+	}
+}
